d38eead2ee86ca0d4c8646c185ac3d1b227d2d28
HCM25_CPL_NET_04_NguyenHoangNhat <nhatnhse182399@fpt.edu.vn>
2025-10-16 00:10:20 +0700

    Add Pages

---
+++ EVCenterService/Pages/Admin/Staff/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Test.Pages.Admin.Staff
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
