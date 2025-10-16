8e1b08304bd4d1ce1ca146603b386a925f594c52
HCM25_CPL_NET_04_NguyenHoangNhat <nhatnhse182399@fpt.edu.vn>
2025-10-16 13:04:58 +0700

    add login register and fix page login register

--- EVCenterService/Pages/Admin/Staff/Index.cshtml.cs
+++ EVCenterService/Pages/Admin/Staff/Index.cshtml.cs
@@ -1,8 +1,10 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Test.Pages.Admin.Staff
 {
+    [Authorize(Roles = "Admin, Staff")]
     public class IndexModel : PageModel
     {
         public void OnGet()
--- EVCenterService/Pages/Customer/Appointments/Index.cshtml.cs
+++ EVCenterService/Pages/Customer/Appointments/Index.cshtml.cs
@@ -1,8 +1,10 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Test.Pages.Customer.Appointments
 {
+    [Authorize(Roles = "Customer")]
     public class IndexModel : PageModel
     {
         public void OnGet()
