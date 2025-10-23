1ef535ec2aaeb01dff8ae5f35bc9a744dc8d04fd
Boisopsop <nhatnhse182399@fpt.edu.vn>
2025-10-23 15:53:03 +0700

    hot fix staff page

--- EVCenterService/Pages/Staff/Appointments/Details.cshtml.cs
+++ EVCenterService/Pages/Staff/Appointments/Details.cshtml.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace Test.Pages.Staff.Appointments
+namespace EVCenterService.Pages.Staff.Appointments
 {
     public class DetailsModel : PageModel
     {
